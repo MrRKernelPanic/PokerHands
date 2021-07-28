ef91ed0f1a3fc204812d785a643f4b013df27e20
Mark Routledge <mark@mroutledge.co.uk>
2021-07-28 21:48:28 +0100

    Add simple test for a flush hand

--- poker_hands.py
+++ poker_hands.py
@@ -1,2 +1,11 @@
 def check_hand(hand):
+    hearts = hand.count("H")   
+    spades = hand.count("S")
+    clubs = hand.count("C")
+    diamonds = hand.count("D")
+
+    if hearts == 5 or spades == 5 or clubs == 5 or diamonds == 5:
+        return "Flush"
+
     raise Exception
+    
--- poker_hands_test.py
+++ poker_hands_test.py
@@ -5,3 +5,7 @@ from poker_hands import check_hand
 def test_raise_exception_if_hand_is_empty():
     with pytest.raises(Exception):
         check_hand("")
+
+def test_flush():
+    hand = "2H 3H 5H 6H 7H"
+    assert check_hand(hand) == "Flush"
