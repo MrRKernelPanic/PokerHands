ffef4763ddcbbd99f19abc004bbc8863f8154ab1
Mark Routledge <mark@mroutledge.co.uk>
2021-08-04 22:10:23 +0100

    Allows 2 player hands

--- poker_hands.py
+++ poker_hands.py
@@ -2,9 +2,20 @@ from card import Card
 from hand import Hand
 
 
-def check_hand(raw_hand):    
-    hand = Hand(raw_hand)
-   
-    return hand.find_poker_hand()
+def check_hand(raw_player_hands):
+    _split = raw_player_hands.split("  ")
+    raw_black_hand = _split[0].replace("Black: ","")
+    raw_white_hand = _split[1].replace("White: ","")
+
+    black_hand = Hand(raw_black_hand)
+    white_hand = Hand(raw_white_hand)
+    
+    black_poker_hand = black_hand.find_poker_hand()
+    white_poker_hand = white_hand.find_poker_hand()
+    
+    print(white_poker_hand)
+
+    if white_poker_hand == "Straight Flush":
+        return "White wins, - with " + white_poker_hand
 
     raise Exception
--- poker_hands_test.py
+++ poker_hands_test.py
@@ -4,3 +4,7 @@ from poker_hands import check_hand
 def test_raise_exception_if_hand_is_empty():
     with pytest.raises(Exception):
         check_hand("")
+
+def test_straight_flush_wins():
+    test_hand = "Black: 2H 3D 5S 9C KD  White: 2H 3H 4H 5H 6H"
+    assert check_hand(test_hand) == "White wins, - with Straight Flush"
